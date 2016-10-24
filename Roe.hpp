00145c89349578bd6f658fff6affc8d7546b2a1e
David Wang <david@cncadmins-MacBook-Air-2.local>
2016-10-24 19:56:51 +0800

    all done but format needed

--- Roe.hpp
+++ Roe.hpp
@@ -15,82 +15,39 @@ class Roe : public NumFormat
 {
 public:
     
-    Roe();
+    Roe(){}
     
-    ~Roe();
+    ~Roe(){}
     
     virtual void output(string) const;
     
-    virtual void setN(long);
+    virtual void solve();
     
     /**
-     *  name    setCFL   set the CFL
-     *  return  void
-     *  param   double   the value
-     *
-     **/
-    void setCFL(double);
-    
-    /**
-     *  name    setTol   set the TOL
+     *  name    setTol   set the contrast entropy correction 对比熵修正
      *  return  void
      *  param   double   the value
      *
      **/
     void setTol(double);
     
-    /**
-     *  name    solve   solve the Roe
-     *  return  void
-     *
-     **/
-    virtual void solve();
-    
 private:
-    
-    /**
-     *  name    setU   set the time temporary value
-     *  return  void
-     *
-     **/
-    void setU();
     
     /**
      *  name    RiemannSolve   solve the Riemann matrix
      *  return  void
      *  param   long   the index numbers in the array
-     *  param   double**   the array(u || u1)
+     *  param   double**   the array(u & u1)
      *  param   double**   the target array(f)
      *
      **/
     void RiemannSolve(long, double**&, double**&);
     
     /**
-     *  CFL
-     */
-    double CFL = 0.9;
-    
-    /**
-     *  time temporary value
-     */
-    double** u = NULL;
-    
-    /**
      *  Contrast entropy correction 对比熵修正
      */
     double tol = 0.000001;
 };
-
-Roe::Roe()
-{
-    construction();
-    setU();
-}
-
-Roe::~Roe()
-{
-    deleteArray(u);
-}
 
 void Roe::output(string filename) const
 {
@@ -110,45 +67,88 @@ void Roe::output(string filename) const
     cout<<"Roe successful  tol="<<tol<<endl;
 }
 
-void Roe::setN(long value)
-{
-    if (n<2) n = 2;
-    else n = value;
-    setDeltaX();
-    reset();
-    setE();
-    setU();
-}
-
-void Roe::setCFL(double value)
-{
-    if (value<0) CFL = 0;
-    else CFL = value;
-}
-
 void Roe::setTol(double value)
 {
     if (value<0) tol = 0;
     else tol = value;
 }
 
-void Roe::setU()
+void Roe::solve()
 {
-    long i;
-    deleteArray(u);
-    newArray(u);
+    long count = 1;     // Main repeat times
+    double tNow = 0;    // Time now (in main repeat)
+    double CFL = 0.9;   // Normal CFL
+    
+    double** u1;        // Runge-Kutta uTemp
+    newArray(u1);
+    double** f;         // Convective numberical flux
+    newArray(f);
+    long i,j;
+    // CFL_new actual CFL; max max number to get the delta time
+    double CFL_new,deltaT,temp,max;
+    
+    for(i=0;i<3;i++)
+        for(j=0;j<n;j++)
+            u1[i][j] = u[i][j];
+    
+    while (tNow<t)
+    {
+        // Get the value of delta t
+        max = 1e-99;
+        for(i=0;i<n;i++)
+        {
+            temp = sqrt(gama*((gama-1.0)*(u[2][i]-0.5*u[1][i]*u[1][i]/u[0][i]))/u[0][i])+fabs(u[1][i]/u[0][i]);
+            if (temp>max) max = temp;
+        }
+        if (count<=10) CFL_new = 0.2*CFL;
+        else CFL_new = CFL;
+        deltaT = CFL_new*deltaX/max;
+        if (tNow+deltaT>t) deltaT=t-tNow;
+        
+        // Use there-order Runge-Kutta to solve the Riemann Problem
+        for(i=0;i<(n-1);i++)
+        {
+            RiemannSolve(i, u, f);
+            for(j=0;j<3;j++)
+                if (i!=0) u1[j][i] = u[j][i]+deltaT/deltaX*(f[j][i-1]-f[j][i]);
+        }
+        for(i=0;i<(n-1);i++)
+        {
+            RiemannSolve(i, u1, f);
+            for(j=0;j<3;j++)
+                if (i!=0) u1[j][i] = 0.75*u[j][i]+0.25*(u1[j][i]+deltaT/deltaX*(f[j][i-1]-f[j][i]));
+        }
+        for(i=0;i<(n-1);i++)
+        {
+            RiemannSolve(i, u1, f);
+            if (i!=0) for(j=0;j<3;j++) u[j][i] = 1.0/3.0*u[j][i]+2.0/3.0*(u1[j][i]+deltaT/deltaX*(f[j][i-1]-f[j][i]));
+        }
+        
+        count++;
+        tNow = tNow+deltaT;
+    }
+    
+    // Get the value of final Velocity(p[0]), Density(p[1]), Pressure(p[2])
+    // and Internal Energy(e)
     for(i=0;i<n;i++)
     {
-        u[0][i] = p[1][i];
-        u[1][i] = p[0][i]*p[1][i];
-        u[2][i] = e[i];
+        p[1][i] = u[0][i];
+        p[0][i] = u[1][i]/u[0][i];
+        e[i] = u[2][i];
+        p[2][i] = (e[i]-1/2*p[1][i]*p[0][i]*p[0][i])*(gama-1);
     }
+    
+    deleteArray(f);
+    deleteArray(u1);
 }
 
 void Roe::RiemannSolve(long index, double**& uTemp, double**& f)
 {
-    double lamda[3],l[6],r[6],s[3],tol,sm[3],sFinal[3];
+    double l[4],r[4],s[3],lamda[3],sm[3],sFinal[3];
+    double tempL,tempR;
     
+    // Left and Right x position status
+    // 0 Density; 1 Velocity; 2 Internal Energy; 3 Pressure
     l[0] = uTemp[0][index];
     l[1] = uTemp[1][index]/uTemp[0][index];
     l[2] = uTemp[2][index];
@@ -157,15 +157,22 @@ void Roe::RiemannSolve(long index, double**& uTemp, double**& f)
     r[1] = uTemp[1][index+1]/uTemp[0][index+1];
     r[2] = uTemp[2][index+1];
     r[3] = (r[2]-0.5*r[0]*r[1]*r[1])*(gama-1);
+    
+    // s[] use to get lamda matrix and get a parameter(h) to calculate f
+    // lamda matrix:
+    //      |  u   0   0  |     lamda[0] = u
+    //      |  0  u+a  0  |     lamda[1] = u+a
+    //      |  0   0  u-a |     lamda[2] = u-a
+    // s[0] u; s[1] a
     s[0] = (sqrt(l[0])*l[1]+sqrt(r[0])*r[1])/(sqrt(l[0])+sqrt(r[0]));;
-    l[4] = l[3]/(gama-1)+0.5*l[0]*l[1]*l[1];
-    r[4] = r[3]/(gama-1)+0.5*r[0]*r[1]*r[1];
-    l[5] = (l[4]+l[3])/l[0];
-    r[5] = (r[4]+r[3])/r[0];
-    s[2] = (sqrt(l[0])*l[5]+sqrt(r[0])*r[5])/(sqrt(l[0])+sqrt(r[0]));
+    tempL = l[3]/(gama-1)+0.5*l[0]*l[1]*l[1];
+    tempR = r[3]/(gama-1)+0.5*r[0]*r[1]*r[1];
+    tempL = (tempL+l[3])/l[0];
+    tempR = (tempR+r[3])/r[0];
+    s[2] = (sqrt(l[0])*tempL+sqrt(r[0])*tempR)/(sqrt(l[0])+sqrt(r[0]));
     s[1] = sqrt((gama-1)*(s[2]-0.5*s[1]*s[1]));
     
-    //Get the eigenvalues
+    // Get the lamda by contrast entropy correction
     if (fabs(s[0])>=tol) lamda[0] = fabs(s[0]);
     else lamda[0]=(s[0]*s[0]+tol*tol)/2.0/tol;
     if (fabs(s[0]+s[1])>=tol) lamda[1] = fabs(s[0]+s[1]);
@@ -173,6 +180,7 @@ void Roe::RiemannSolve(long index, double**& uTemp, double**& f)
     if (fabs(s[0]-s[1])>=tol) lamda[2] = fabs(s[0]-s[1]);
     else lamda[2]=((s[0]-s[1])*(s[0]-s[1])+tol*tol)/2.0/tol;
     
+    // Matrix operation (expand and simplify)
     double S[3][3] = {1,1,1,s[0],s[0]+s[1],s[0]-s[1],0.5*s[0]*s[0],s[2]+s[0]*s[1],s[2]-s[0]*s[1]};
     sm[0] = (gama-1)/(s[1]*s[1])*((r[0]-l[0])*(s[2]-s[0]*s[0])+s[0]*(r[1]*r[0]-l[1]*l[0])-(r[2]-l[2]));
     sm[1]=0.5/s[1]*((r[0]-l[0])*(-s[0]+s[1])+(r[1]*r[0]-l[1]*l[0])-s[1]*sm[0]);
@@ -183,67 +191,11 @@ void Roe::RiemannSolve(long index, double**& uTemp, double**& f)
     sFinal[0] = S[0][0]*lamda[0]+S[0][1]*lamda[1]+S[0][2]*lamda[2];
     sFinal[1] = S[1][0]*lamda[0]+S[1][1]*lamda[1]+S[1][2]*lamda[2];
     sFinal[2] = S[2][0]*lamda[0]+S[2][1]*lamda[1]+S[2][2]*lamda[2];
+    
+    // Get convective numberical flux
     f[0][index] = 0.5*(l[0]*l[1]+r[0]*r[1]-sFinal[0]);
     f[1][index] = 0.5*(l[0]*l[1]*l[1]+l[3]+r[0]*r[1]*r[1]+r[3]-sFinal[1]);
     f[2][index] = 0.5*((l[2]+l[3])*l[1]+(r[2]+r[3])*r[1]-sFinal[2]);
-}
-
-void Roe::solve()
-{
-    long i,j;
-    double** u1;
-    newArray(u1);
-    double* a = new double[n];
-    double** f;
-    newArray(f);
-    long count = 1;
-    double tNow = 0;
-    double max = 0;
-    double CFL_new,deltaT;
-    for(i=0;i<3;i++)
-        for(j=0;j<n;j++)
-            u1[i][j] = u[i][j];
-    
-    while (tNow<t)
-    {
-        for(i=0;i<n;i++) a[i]=sqrt(gama*p[2][i]/p[1][i]);
-        for(i=0;i<n;i++)
-            if ((fabs(p[0][i])+a[i])>max) max = fabs(p[0][i])+a[i];
-        if (count<=10) CFL_new = 0.2*CFL;
-        else CFL_new = CFL;
-        deltaT = CFL_new*deltaX/max;
-        if (tNow+deltaT>t) deltaT=t-tNow;
-    
-        // there-order Runge-Kutta
-        RiemannSolve(0, u, f);
-        for(i=1;i<(n-1);i++)
-        {
-            RiemannSolve(i, u, f);
-            for(j=0;j<3;j++) u1[j][i] = u[j][i]+deltaT/deltaX*(f[j][i-1]-f[j][i]);
-        }
-        RiemannSolve(0, u1, f);
-        for(i=1;i<(n-1);i++)
-        {
-            RiemannSolve(i, u1, f);
-            for(j=0;j<3;j++) u1[j][i] = 0.75*u[j][i]+0.25*(u1[j][i]+deltaT/deltaX*(f[j][i-1]-f[j][i]));
-        }
-        RiemannSolve(0, u1, f);
-        for(i=1;i<(n-1);i++)
-        {
-            RiemannSolve(i, u1, f);
-            for(j=0;j<3;j++) u[j][i] = 1.0/3.0*u[j][i]+2.0/3.0*(u1[j][i]+deltaT/deltaX*(f[j][i-1]-f[j][i]));
-            p[0][i] = u[1][i]/u[0][i];
-            p[1][i] = u[0][i];
-            e[i] = u[2][i];
-            p[2][i] = (e[i]-1/2*p[1][i]*p[0][i]*p[0][i])*(gama-1);
-        }
-        tNow = tNow+deltaT;
-        count++;
-    }
-    
-    deleteArray(f);
-    delete [] a;
-    deleteArray(u1);
 }
 
 #endif /* Roe_hpp */
--- main.cpp
+++ main.cpp
@@ -1,23 +1,21 @@
 /**
  *  main.cpp
  *  the main procedure to use c++ get the data and use matlab to plot the image
+ *  all the number format using three-order Runge-Kutta
  *
  *  Created by David Wang on 16/10/17.
  *
  */
 
-#include <iostream>
 #include "Roe.hpp"
 #include "StegerWarming.hpp"
 #include "AUSM.hpp"
 
-/**
- *  output file path
- */
+// The output full file path. Please change to your code path before running.
 const string FILEPATH="/Users/cncuser/Desktop/c_code/CFD2/CFD2/";
 
 int main(int argc, const char * argv[]) {
-    /*Roe roe;
+    Roe roe;
     roe.setFilePath(FILEPATH);
     roe.solve();        //normal tol=0.000001
     roe.output("Roe1.txt");
@@ -37,9 +35,11 @@ int main(int argc, const char * argv[]) {
     StegerWarming sw;
     sw.setFilePath(FILEPATH);
     sw.solve();
-    sw.output("StegerWarming.txt");*/
+    sw.output("StegerWarming.txt");
     AUSM ausm;
     ausm.setFilePath(FILEPATH);
+    ausm.solve();
+    ausm.output("AUSM.txt");
     
     return 0;
 }
